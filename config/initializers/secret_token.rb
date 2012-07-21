3b6b0b3d3e2118e5b8725553e193b0a1034979e4
森山 雄貴 <moriyamayuuki@moriyama-yuki-no-MacBook-Pro.local>
2012-07-22 01:32:27 +0900

    first_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musirec::Application.config.secret_token = '2129d433bab6f35bca57ac6ee3a33b54578ddcba14841ecf2918410a0613c670c98d07363423d98f484a56dfcc562985d1c21a1cff161504b929a0491303859b'
